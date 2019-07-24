9f081eb697c429fc5240c047d330003c9f74c63f
Salomé Pelaez <spsalome@gmail.com>
2019-07-24 16:20:58 -0500

    Adds HeroMove class

---
+++ Survival/Assets/HeroMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+    public GameObject camara;
+    private float speed = 0.1f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed;
+        }
+
+        float rotacion = camara.transform.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(0.0f, rotacion, 0.0f);
+
+    }
+}
