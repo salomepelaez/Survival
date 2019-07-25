fe7cae84640e016635e63283c67afd473dd82e7b
Salomé Pelaez <spsalome@gmail.com>
2019-07-25 13:31:41 -0500

    Edits HeroMove class

--- Survival/Assets/Scenes/HeroMove.cs
+++ Survival/Assets/Scenes/HeroMove.cs
@@ -2,34 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeroMove : MonoBehaviour
+public class HeroMove : Builder
 {
-    public GameObject camara;
-    private float speed = 0.1f;
+    
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * speed;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * speed;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * speed;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position -= transform.right * speed;
-        }
-
-        float rotacion = camara.transform.eulerAngles.y;
-        transform.rotation = Quaternion.Euler(0.0f, rotacion, 0.0f);
-
+        Hero();
     }
 }
