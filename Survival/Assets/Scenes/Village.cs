0c5eb1a832c0cc28e0da1f7ce171e89c3f6cd9d2
Salomé Pelaez <spsalome@gmail.com>
2019-07-25 13:33:53 -0500

    Edits Village class

--- Survival/Assets/Scenes/Village.cs
+++ Survival/Assets/Scenes/Village.cs
@@ -2,32 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Village : MonoBehaviour
+public class Village : Builder
 {
+
     public void Start()
     {
         Creacion();
     }
 
-    public GameObject[] ald;
 
-    public void Creacion()
-    {
-        ald = new GameObject[10];
-        int villagers = Random.Range(3, 6);
-        
-
-        for (int j = 0; j < villagers; j++)
-        {
-            GameObject villager = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Vector3 posicion = new Vector3();
-            posicion.x = Random.Range(-30, 30);
-            posicion.z = Random.Range(-30, 30);
-            villager.transform.position = posicion;
-            Debug.Log("aldeano");
-
-            
-        }
-
-    }
 }
