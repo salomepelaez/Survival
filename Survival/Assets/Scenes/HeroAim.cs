1d4446bc395a6423c45944fbe6f84994e1cf7e6b
Salomé Pelaez <spsalome@gmail.com>
2019-07-25 12:37:39 -0500

    Adds HeroAim class

---
+++ Survival/Assets/Scenes/HeroAim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAim : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+    public bool invertedMouse;
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X");
+        if (invertedMouse)
+        {
+            mouseY += Input.GetAxis("Mouse Y");
+        }
+        else
+            mouseY -= Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+
+    
+}
