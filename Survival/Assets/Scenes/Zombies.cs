f0580af612f34fbacfc0332fb31ef8bbad9b24c4
Salomé Pelaez <spsalome@gmail.com>
2019-07-25 17:35:47 -0500

    Adds builder to Zombie class

--- Survival/Assets/Scenes/Zombies.cs
+++ Survival/Assets/Scenes/Zombies.cs
@@ -2,13 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zombies : Builder
+public class Zombies : MonoBehaviour
 {
     public void Start()
-    {        
-       Zombie();
+    {
+        int n = Random.Range(2, 6);
+        Zombie z = new Zombie(n);
     }
 
+}
+
+public class Zombie
+{
+    public int myZombie = Random.Range(2, 6);
+    public Zombie(int myZombie)
+    {
+        this.myZombie = myZombie;
+        for (int i = 0; i < myZombie; i++)
+        {
+            GameObject zombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Vector3 posicion = new Vector3();
+            posicion.x = Random.Range(-30, 30);
+            posicion.z = Random.Range(-30, 30);
+            zombie.transform.position = posicion;
+
+            int color = Random.Range(0, 3);
+
+            switch (color)
+            {
+                case 0:
+                    zombie.GetComponent<Renderer>().material.color = Color.cyan;
+                    break;
+                case 1:
+                    zombie.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 2:
+                    zombie.GetComponent<Renderer>().material.color = Color.magenta;
+                    break;
+            }
+        }
+
+        Debug.Log("zombie");
+    }
 }
 
 
