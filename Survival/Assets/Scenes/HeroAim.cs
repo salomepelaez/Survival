92093df6fe625bd27c4740e986757aa763ac5550
Salomé Pelaez <spsalome@gmail.com>
2019-07-25 13:32:03 -0500

    Edits HeroAim class

--- Survival/Assets/Scenes/HeroAim.cs
+++ Survival/Assets/Scenes/HeroAim.cs
@@ -2,22 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeroAim : MonoBehaviour
+public class HeroAim : Builder
 {
-    float mouseX;
-    float mouseY;
-    public bool invertedMouse;
 
     void Update()
     {
-        mouseX += Input.GetAxis("Mouse X");
-        if (invertedMouse)
-        {
-            mouseY += Input.GetAxis("Mouse Y");
-        }
-        else
-            mouseY -= Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+        CameraRot();
     }
 
     
