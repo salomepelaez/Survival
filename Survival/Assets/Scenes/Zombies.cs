c9bfe9b83433d9d1e12eab153e61544aa4bdcf70
Salomé Pelaez <spsalome@gmail.com>
2019-07-29 15:51:48 -0500

    Changes Village class

--- Survival/Assets/Scenes/Zombies.cs
+++ Survival/Assets/Scenes/Zombies.cs
@@ -4,59 +4,70 @@ using UnityEngine;
 
 public class Zombies : MonoBehaviour
 {
+    int n;
+    int nZombies;
+    int v;
+
     public void Start()
     {
-        int n = Random.Range(2, 6);
-        Zombie z = new Zombie(n);
+        
+        n = Random.Range(4, 10);
+        nZombies = Random.Range(1, (n - 1));
+        v = n - nZombies;
+        Zombie z = new Zombie(nZombies);
         
     }
-    
+
 }
 
 public class Zombie
 {
-    public int myZombie = Random.Range(2, 6);
-    
+    public GameObject[] zom;
+    public int myZombie = Random.Range(4, 10);
     public Zombie(int myZombie)
     {
         this.myZombie = myZombie;
+        zom = new GameObject[10];
+
         for (int i = 0; i < myZombie; i++)
         {
-            GameObject zombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            GameObject zZombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
             Vector3 posicion = new Vector3();
             posicion.x = Random.Range(-30, 30);
             posicion.z = Random.Range(-30, 30);
-            zombie.transform.position = posicion;
+            zZombie.transform.position = posicion;
 
             int color = Random.Range(0, 3);
 
             switch (color)
             {
                 case 0:
-                    zombie.GetComponent<Renderer>().material.color = Color.cyan;
+                    zZombie.GetComponent<Renderer>().material.color = Color.cyan;
                     break;
                 case 1:
-                    zombie.GetComponent<Renderer>().material.color = Color.green;
+                    zZombie.GetComponent<Renderer>().material.color = Color.green;
                     break;
                 case 2:
-                    zombie.GetComponent<Renderer>().material.color = Color.magenta;
+                    zZombie.GetComponent<Renderer>().material.color = Color.magenta;
                     break;
             }
 
-            if(zombie.GetComponent<Renderer>().material.color == Color.cyan)
+            if(zZombie.GetComponent<Renderer>().material.color == Color.cyan)
             {
                 Debug.Log("Cerebrossssss color Cyan. Grrr");
             }
 
-            if (zombie.GetComponent<Renderer>().material.color == Color.green)
+            if (zZombie.GetComponent<Renderer>().material.color == Color.green)
             {
                 Debug.Log("Zombie verrrde");
             }
 
-            if (zombie.GetComponent<Renderer>().material.color == Color.magenta)
+            if (zZombie.GetComponent<Renderer>().material.color == Color.magenta)
             {
                 Debug.Log("Zombie magenta. Cerebrosss");
             }
+
+            zom[i] = zZombie;
         }
 
         
