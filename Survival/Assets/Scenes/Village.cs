425d63558a7bf4490d42781c4f614e90b108cdca
Salomé Pelaez <spsalome@gmail.com>
2019-07-31 13:45:56 -0500

    Changes Village class

--- Survival/Assets/Scenes/Village.cs
+++ Survival/Assets/Scenes/Village.cs
@@ -7,13 +7,11 @@ public class Village : MonoBehaviour
 
     public void Start()
     {
-
-
-        int age = Random.Range(15, 100);
+        
         int n = Random.Range(4, 10);
         int nZombies = Random.Range(1, (n - 1));       
         int v = n - nZombies;
-
+        
         string[] names = new string[20]
         {
                 "Rose",
@@ -38,9 +36,23 @@ public class Village : MonoBehaviour
                 "David",
         };
 
-        Villager myVillager = new Villager(v);
-        Villager myVillager2 = new Villager(age, names);
+ 
+        for (int j = 0; j < v; j++)
+        {
+            new Villager(Random.Range(15, 101), names[Random.Range(0, names.Length)]);
+           
+            Message();
+        }
+                
+        void Message()
+        {
+            Debug.Log("¡Hola, citadino! Me llamo " + (names[v]) + ". Y tengo  " + Random.Range(15, 101) + " años.");
+        }
+
+       
     }
+
+    
 }
 
 public class Villager
@@ -48,36 +60,19 @@ public class Villager
     private string[] names;
     public GameObject[] ald;
     int villagers = Random.Range(4, 10);
-    int age = Random.Range(15, 100);
 
-    public Villager(int villagers)
+    public Villager(int villagers, string names)
     {
         this.villagers = villagers;
         ald = new GameObject[10];
-
-        for (int j = 0; j < villagers; j++)
-        {
+             
             GameObject villager = GameObject.CreatePrimitive(PrimitiveType.Cube);
             Vector3 posicion = new Vector3();
             posicion.x = Random.Range(-30, 30);
             posicion.z = Random.Range(-30, 30);
-            villager.transform.position = posicion;
-            
-            ald[j] = villager;
-        }
+            villager.transform.position = posicion;          
+                    
     }
-
-
-    public Villager(int age, string[] names)
-    {
-        this.age = age;
-        this.names = names;
-        int n = Random.Range(4, 10);
-        int nZombies = Random.Range(1, (n - 1));
-        int v = n - nZombies;
-
-        Debug.Log("¡Hola, citadino! Me llamo " + (names[v]) + ". Y tengo  " + age + " años.");
-    }
-
-    //Hasta acá funciona pero solo imprime una vez.
+    
+   //Hasta acá funciona pero le imprime el mismo nombre y edad a todos xd
 }
