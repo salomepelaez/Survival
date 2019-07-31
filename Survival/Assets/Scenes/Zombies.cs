8a926a12196ecbc38673226c1f10f02e782c4bd2
Salomé Pelaez <spsalome@gmail.com>
2019-07-31 14:19:55 -0500

    Changes Zombie class

--- Survival/Assets/Scenes/Zombies.cs
+++ Survival/Assets/Scenes/Zombies.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class Zombies : MonoBehaviour
 {
-    int n;
-    int nZombies;
-    int v;
 
     public void Start()
     {
         
-        n = Random.Range(4, 10);
-        nZombies = Random.Range(1, (n - 1));
-        v = n - nZombies;
-        Zombie z = new Zombie(nZombies);
+        int n = Random.Range(4, 10);
+        int  nZombies = Random.Range(1, (n - 1));
+        int v = n - nZombies;
         
+        for (int i = 0; i < nZombies; i++)
+        {
+            new Zombie(n);
+        }
+
+
     }
 
 }
@@ -29,46 +31,44 @@ public class Zombie
         this.myZombie = myZombie;
         zom = new GameObject[10];
 
-        for (int i = 0; i < myZombie; i++)
+        GameObject zZombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Vector3 posicion = new Vector3();
+        posicion.x = Random.Range(-30, 30);
+        posicion.z = Random.Range(-30, 30);
+        zZombie.transform.position = posicion;
+
+        int color = Random.Range(0, 3);
+
+        switch (color)
         {
-            GameObject zZombie = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Vector3 posicion = new Vector3();
-            posicion.x = Random.Range(-30, 30);
-            posicion.z = Random.Range(-30, 30);
-            zZombie.transform.position = posicion;
-
-            int color = Random.Range(0, 3);
-
-            switch (color)
-            {
-                case 0:
-                    zZombie.GetComponent<Renderer>().material.color = Color.cyan;
-                    break;
-                case 1:
-                    zZombie.GetComponent<Renderer>().material.color = Color.green;
-                    break;
-                case 2:
-                    zZombie.GetComponent<Renderer>().material.color = Color.magenta;
-                    break;
-            }
-
-            if(zZombie.GetComponent<Renderer>().material.color == Color.cyan)
-            {
-                Debug.Log("Cerebrossssss color Cyan. Grrr");
-            }
-
-            if (zZombie.GetComponent<Renderer>().material.color == Color.green)
-            {
-                Debug.Log("Zombie verrrde");
-            }
-
-            if (zZombie.GetComponent<Renderer>().material.color == Color.magenta)
-            {
-                Debug.Log("Zombie magenta. Cerebrosss");
-            }
-
-            zom[i] = zZombie;
+            case 0:
+                zZombie.GetComponent<Renderer>().material.color = Color.cyan;
+                break;
+            case 1:
+                zZombie.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case 2:
+                zZombie.GetComponent<Renderer>().material.color = Color.magenta;
+                break;
         }
+
+        if(zZombie.GetComponent<Renderer>().material.color == Color.cyan)
+        {
+            Debug.Log("Cerebrossssss color Cyan. Grrr");
+        }
+
+        if (zZombie.GetComponent<Renderer>().material.color == Color.green)
+        {
+            Debug.Log("Zombie verrrde");
+        }
+
+        if (zZombie.GetComponent<Renderer>().material.color == Color.magenta)
+        {
+            Debug.Log("Zombie magenta. Cerebrosss");
+        }
+
+            
+        
 
         
     }
