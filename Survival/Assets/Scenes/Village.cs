1a2ba77987cd80cf3f9f82ddc3cf336b4f28d583
Salomé Peláez <spsalome@gmail.com>
2019-08-01 13:21:48 -0500

    Changes Village class

--- Survival/Assets/Scenes/Village.cs
+++ Survival/Assets/Scenes/Village.cs
@@ -10,59 +10,70 @@ public class Village : MonoBehaviour
     {        
         GameObject s = Instantiate(held, Vector3.zero, Quaternion.identity); //Se instanció el GameObject como un prefab.
 
-        int n = Random.Range(4, 10); //El Random "n" es el que se encarga de escoger un número aleatorio entre 4 y 9, pues el 10 nolo toma en cuenta.       
+        int n = Random.Range(4, 9); //El Random "n" es el que se encarga de escoger un número aleatorio entre 4 y 9, pues el 10 nolo toma en cuenta.       
         int nZombies = Random.Range(1, (n - 1));  //El número Random para Zombies es escogido entre 1 y la resta del primer Random menos uno.
         int v = n - nZombies; //El número de Villagers es escogido mediante la operación de resta entre el primer Random y el Random de Zombies.
+        
+        string[] names = new string[20]; //Se creó una matriz que almacena todos los nombres.
 
-        string[] names = new string[20] //Se creó una matriz que almacena todos los nombres.
-        {
-                "Rose",
-                "Ophelie",
-                "Celeste",
-                "Mérida",
-                "Catrina",
-                "Dean",
-                "Will",
-                "Lucas",
-                "Dustin",
-                "Mike",
-                "Sophie",
-                "Isabella",
-                "Amelie",
-                "Charlotte",
-                "Milo",
-                "Dante",
-                "Ariel",
-                "Suhail",
-                "Jake",
-                "David",
-        };
 
-         //El siguiente bloque de código instancia los objetos, en este caso los Villagers.
+        names[0] = "Rose";
+        names[1] = "Ophelie";
+        names[2] = "Celeste";
+        names[3] = "Mérida";
+        names[4] = "Catrina";
+        names[5] = "Dean";
+        names[6] = "Will";
+        names[7] = "Lucas";
+        names[8] = "Dustin";
+        names[9] = "Mike";
+        names[10] = "Sophie";
+        names[11] = "Isabella";
+        names[12] = "Amelie";
+        names[13] = "Charlotte";
+        names[14] = "Milo";
+        names[15] = "Dante";
+        names[16] = "Ariel";
+        names[17] = "Suhail";
+        names[18] = "Jake";
+        names[19] = "David";
+        
+
+        //El siguiente bloque de código instancia los objetos, en este caso los Villagers.
         for (int j = 0; j < v; j++)
         {            
-            new Villager(Random.Range(15, 101), names[Random.Range(0, names.Length)]);          
-        }            
+            new Villager(v, names[Random.Range(0, names.Length)]);          
+        }
+
+        //El siguiente bloque de código es el que instancia los objetos, en este caso los zombies.
+        for (int i = 0; i < nZombies; i++)
+        {
+            new Zombie(n);
+        }
     }    
 }
 
 public class Villager //En esta clase secundaria se encuentra el constructor.
 {
     string names; //Se declaró un string que luego se utilizará en la función.
-    
-    int villagers = Random.Range(4, 10);
+    public GameObject[] ald;
+    int villagers = Random.Range(4, 9);
 
     public Villager(int villagers, string names)  //En este caso el constructor debe recibir parámetros tanto del tipo Entero como del tipo Int.
     {
         this.villagers = villagers;
         this.names = names;
-        
+        ald = new GameObject[10];
+
         //El siguiente bloque de código genera los objetos y les brinda una ubicación aleatoria.
         GameObject villager = GameObject.CreatePrimitive(PrimitiveType.Cube);
         Vector3 posicion = new Vector3();
         posicion.x = Random.Range(-30, 30);
         posicion.z = Random.Range(-30, 30);
         villager.transform.position = posicion;
+
+        ald[villagers] = villager;
+
         Debug.Log(Message()); //Imprime la función.
     }
 
